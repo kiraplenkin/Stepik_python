0431eb02c2a54d2f730f4d7e87ed6affd59bd7f8
Строганов Кирилл Дмитриевич <stroganov-kd@fsrar.ru>
2019-04-15 11:38:45 +0300

    Update

--- 1.3.py
+++ 1.3.py
@@ -64,3 +64,20 @@ def closest_mod_5(x):
             return x
         else:
             x += 1
+
+
+############################
+print('\nTask 14')
+
+
+def C(n, k):
+    if k > n:
+        return 0
+    elif k == 0:
+        return 1
+    else:
+        return C(n - 1, k) + C(n - 1, k - 1)
+
+
+n, k = map(int, input().split())
+print(C(n, k))
