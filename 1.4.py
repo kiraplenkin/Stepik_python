3362a049b7d30631cddbdf323a424745db2e620f
Строганов Кирилл Дмитриевич <stroganov-kd@fsrar.ru>
2019-04-15 17:52:11 +0300

    Update

--- 1.4.py
+++ 1.4.py
@@ -1,11 +1,33 @@
 ############################
 print('\nTask 9')
 
+# create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
+# add <namespace> <var> – добавить в пространство <namespace> переменную <var>
+# get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из
+# пространства <namespace>, или None, если такого пространства не существует
+
+# add global a
+# create foo global
+# add foo b
 
 n = int(input())
-dict = {}
+
+parent_namespaces = {
+    'None': 'global'
+}
+
+child_namespaces = {
+    'global': []
+}
+
 for i in range(n):
     operation, namespace, arg = input().split()
+    if operation == 'create':
+        pass child_namespaces[arg]
+    elif operation == 'add':
+        pass # child_namespaces[namespace] = arg
+    elif operation == 'get':
+        pass
 
-print(dict.items())
+    print(child_namespaces.items())
 
