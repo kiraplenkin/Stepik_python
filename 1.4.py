772bfd9199c5db261f8b074fcb54248a1fef4bc8
stroganov-kd <myp2zz@yandex.ru>
2019-04-16 18:15:51 +0300

    Update

--- 1.4.py
+++ 1.4.py
@@ -8,16 +8,19 @@ def add(namespace, var):
 
 
 def get(namespace, var):
-    if var in scopes[namespace]['variables']:
-        return print(namespace)
-    else:
-        get(scopes[namespace]['parent'], var)
+    try:
+        if var in scopes[namespace]['variables']:
+            return print(namespace)
+        else:
+            get(scopes[namespace]['parent'], var)
+    except:
+        return print(None)
 
 
 scopes = {'global': {'parent': None,
                      'variables': set()}}
 
-n = int(input('Set a number: '))
+n = int(input())
 for i in range(n):
     operation, namespace, var = input().split()
     if operation == 'create':
