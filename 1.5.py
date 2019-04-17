0306f7abc2443db7841b23cf8c86ba3aa628f951
stroganov-kd <myp2zz@yandex.ru>
2019-04-17 15:49:25 +0300

    Update

--- 1.5.py
+++ 1.5.py
@@ -1,5 +1,6 @@
 # Task 7
 
+
 class MoneyBox:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -17,4 +18,32 @@ class MoneyBox:
 
 
 # Task 8
+
+class Buffer:
+    def __init__(self):
+        self.count = 0
+        self.sum = 0
+        self.buf = []
+
+    def add(self, *a):
+        for i in range(len(a)):
+            if self.count < 5:
+                self.buf.append(a[i])
+                self.count += 1
+            else:
+                self.sum = 0
+                for n in range(len(self.buf)):
+                    self.sum += self.buf[n]
+                print(self.sum)
+                self.buf = []
+                self.count = 0
+                self.sum = 0
+
+    def get_current_part(self):
+        return print(self.buf)  # ! Change to return self.buf
+
+
+x = Buffer()
+x.add(1, 2, 3, 4, 5, 6, 7)
+x.get_current_part()
 
