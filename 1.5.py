d89a9a77ff0c7f0b828379fadf43ed781b6f0ed2
stroganov-kd <myp2zz@yandex.ru>
2019-04-17 12:16:16 +0300

    Add 1.5 solution

---
+++ 1.5.py
@@ -0,0 +1,20 @@
+# Task 7
+
+class MoneyBox:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.moneys = 0
+
+    def can_add(self, v):
+        if self.moneys + v <= self.capacity:
+            return True
+        else:
+            return False
+
+    def add(self, v):
+        if self.can_add(v):
+            self.moneys += v
+
+
+# Task 8
+
