5fae6d0c2c8d2dbd8b5691ceb5171bbe9dfde260
stroganov-kd <myp2zz@yandex.ru>
2019-04-19 13:22:06 +0300

    Update

--- 1.6.5.py
+++ 1.6.5.py
@@ -1,19 +1,47 @@
+classes = {}
+
+
+def add_class(classes, class_name, parents):
+    if class_name not in classes:
+        classes[class_name] = []
+    classes[class_name].extend(parents)
+    for parent in parents:
+        if parent not in classes:
+            classes[parent] = []
+
+
+def found_path(classes, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in classes:
+        return None
+    for node in classes[start]:
+        if node not in path:
+            newpath = found_path(classes, node, end, path)
+            if newpath:
+                return newpath
+    return None
+
+
+def answer(classes, parent, child):
+    if not (parent or child) in classes or not found_path(classes, child, parent):
+        return 'No'
+    return 'Yes'
+
+
 n = int(input())
 
-lst_mro = []
-lst_q = []
+for _ in range(n):
+    class_description = input().split()
+    class_name = class_description[0]
+    class_parents = class_description[2:]
+    add_class(classes, class_name, class_parents)
 
-for i in range(n):
-    lst_mro.append(input().split(':'))
+q = int(input())
 
-k = int(input())
-
-for i in range(k):
-    lst_q.append(input().split())
-
-
-print(lst_mro[1][1])
-print(lst_q)
-
-for i in range(len(lst_q)):
-    pass
+for _ in range(q):
+    question = input().split()
+    parent = question[0]
+    child = question[1]
+    print(answer(classes, parent, child))
