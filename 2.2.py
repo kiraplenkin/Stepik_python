02698b60347c6478c7196a01a70599939e1088e2
kirr <kiraplenkin@gmail.com>
2019-04-20 14:43:51 +0300

    Update

--- 2.2.py
+++ 2.2.py
@@ -1,3 +1,14 @@
 import exceptions_2_2
 
 print(exceptions_2_2.greet('Kirr'))
+
+#  Task 4
+
+import datetime
+
+data = datetime.datetime.strptime(input(), "%Y %m %d")
+
+new_day = datetime.timedelta(days=int(input()))
+delta = data + new_day
+
+print(delta.strftime("%Y %-m %-d"))
